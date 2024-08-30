c0cd142ae15c7e59eb5088e6a0fc302643c4d707
Jace Randolph <jrandolphacliff@gmail.com>
2024-08-29 18:34:47 -0600

    started to build addTodo function

--- js/script.js
+++ js/script.js
@@ -1,35 +1,38 @@
+const todoInput = document.querySelector('.todo-input');
+
+
 const todos = [
     {
         todoId: 0,
-        todoName: 'Go to the store',
+        todoText: 'Go to the store',
         todoComplete: false,
         category: 'Chore',
         dueDate: '01-05-2024'
     },
     {
         todoId: 1,
-        todoName: 'Mow the lawn',
+        todoText: 'Mow the lawn',
         todoComplete: true,
         category: 'Chore',
         dueDate: '06-15-2024'
     },
     {
         todoId: 2,
-        todoName: 'Create my todo project',
+        todoText: 'Create my todo project',
         todoComplete: false,
         category: 'Homework',
         dueDate: '09-10-2024'
     },
     {
         todoId: 3,
-        todoName: 'Watch movie with wife',
+        todoText: 'Watch movie with wife',
         todoComplete: false,
         category: 'Fun',
         dueDate: '09-14-2024'
     },
     {
         todoId: 4,
-        todoName: 'Go camping over labor day weekend',
+        todoText: 'Go camping over labor day weekend',
         todoComplete: false,
         category: 'Fun',
         dueDate: '09-01-2024'
@@ -38,7 +41,19 @@ const todos = [
 
 
 const addTodo = () => {
+    const todoText = todoInput.value.trim();
 
+    if (todoText) {
+        const newTodo = {
+            todoId: todos.length,
+            todoText: todoText,
+        }
+
+        todos.push(newTodo);
+        console.log(todos);
+
+        todoInput.value = '';
+    }
 }
 
 const deleteTodo = () => {
