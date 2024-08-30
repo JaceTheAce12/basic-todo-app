d849dbb7fe731433cc4abb6671f7cd3b9cb915e4
Jace Randolph <jrandolphacliff@gmail.com>
2024-08-29 18:26:39 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const todos = [
+    {
+        todoId: 0,
+        todoName: 'Go to the store',
+        todoComplete: false,
+        category: 'Chore',
+        dueDate: '01-05-2024'
+    },
+    {
+        todoId: 1,
+        todoName: 'Mow the lawn',
+        todoComplete: true,
+        category: 'Chore',
+        dueDate: '06-15-2024'
+    },
+    {
+        todoId: 2,
+        todoName: 'Create my todo project',
+        todoComplete: false,
+        category: 'Homework',
+        dueDate: '09-10-2024'
+    },
+    {
+        todoId: 3,
+        todoName: 'Watch movie with wife',
+        todoComplete: false,
+        category: 'Fun',
+        dueDate: '09-14-2024'
+    },
+    {
+        todoId: 4,
+        todoName: 'Go camping over labor day weekend',
+        todoComplete: false,
+        category: 'Fun',
+        dueDate: '09-01-2024'
+    },
+]
+
+
+const addTodo = () => {
+
+}
+
+const deleteTodo = () => {
+
+}
