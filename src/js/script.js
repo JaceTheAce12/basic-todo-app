dadc9b9e5ae41a3d721c6230a8a1bf1483d5e4b5
Jace Randolph <122485977+JaceTheAce12@users.noreply.github.com>
2024-10-15 21:56:35 -0600

    linked my todo app to my api server with the todo data

--- src/js/script.js
+++ src/js/script.js
@@ -43,6 +43,19 @@ let todos = [
     },
 ]
 
+const getTodos = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/todos');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Call fetchTodos when the script loads
+getTodos();
+
 const addTodo = () => {
     const todoText = todoInput.value.trim();
     const categoryText = selectCategory.options[selectCategory.selectedIndex].text;
